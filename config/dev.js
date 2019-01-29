0e364d857a5a56dc6d0e2540e37f07a56e8d668d
Steven Thomason <thomason84@hotmail.com>
2019-01-28 22:33:56 -0500

    finished oauth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "849438327500-e3d8ss0otj7d1lt2mgv5k3ecdvi8lc6e.apps.googleusercontent.com",
+  googleClientSecret: "v4muWrAn3WDrIDYXBgeC3A2u",
+  mongoURI: "mongodb://thomason84:mdhttr18@ds113765.mlab.com:13765/emailer-dev",
+  cookieKey: "qebgrpq83yti3ub4t93g938039hg93g"
+};
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  googleClientID:
-    "849438327500-e3d8ss0otj7d1lt2mgv5k3ecdvi8lc6e.apps.googleusercontent.com",
-  googleClientSecret: "v4muWrAn3WDrIDYXBgeC3A2u",
-  mongoURI: "mongodb://thomason84:mdhttr18@ds113765.mlab.com:13765/emailer-dev",
-  cookieKey: "qebgrpq83yti3ub4t93g938039hg93g"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
